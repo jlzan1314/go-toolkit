19bb54259cac6791e2fadddb781b59f18205bbcd
clive <jlzan1314@163.com>
2020-09-27 15:10:00 +0800

    添加驼峰互转

--- string/helper.go
+++ string/helper.go
@@ -2,7 +2,9 @@ package string
 
 import (
 	"math/rand"
+	"strings"
 	"time"
+	"unicode"
 )
 
 func GetRandomString(length int) string {
@@ -14,4 +16,42 @@ func GetRandomString(length int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+func Camel2Snake(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.Append('_')
+			}
+			buffer.Append(unicode.ToLower(r))
+		} else {
+			buffer.Append(r)
+		}
+	}
+	return buffer.String()
+}
+
+// 下划线写法转为驼峰写法
+func Snake2Camel(name string) string {
+	name = strings.Replace(name, "_", " ", -1)
+	name = strings.Title(name)
+	return strings.Replace(name, " ", "", -1)
+}
+
+// 首字母大写
+func Ucfirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToUpper(v)) + str[i+1:]
+	}
+	return ""
+}
+
+// 首字母小写
+func Lcfirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToLower(v)) + str[i+1:]
+	}
+	return ""
 }
